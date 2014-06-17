b21741112afd42435cd7071cf9163940dd2b94e6
Narudom Techaval <narudom.techaval@thomsonreuters.com>
2014-06-17 09:08:37 +0700

    fix ngenter, remove a

--- app/scripts/directives/ngenter.js
+++ app/scripts/directives/ngenter.js
@@ -5,12 +5,14 @@ angular.module('worldcupApp')
   .directive('ngEnter', function () {
     return {
       link: function (scope, element, attrs) {
-        element.bind('blur', function (event) {
-          scope.$apply(function () {
-            scope.$eval(attrs.ngEnter);
-          });
+        element.bind('blur keydown', function (event) {
+          if (event.type === 'blur' || event.which === 13) {
+            scope.$apply(function () {
+              scope.$eval(attrs.ngEnter);
+            });
 
-          event.preventDefault();
+            event.preventDefault();
+          }
         });
       }
     };
