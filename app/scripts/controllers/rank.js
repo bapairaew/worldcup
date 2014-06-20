5ae2ca4a1dd9cb204793713d236bad802341b7cc
Narudom Techaval <narudom.techaval@thomsonreuters.com>
2014-06-20 07:48:24 +0700

    avg chart

--- app/scripts/controllers/rank.js
+++ app/scripts/controllers/rank.js
@@ -4,7 +4,8 @@ angular.module('worldcupApp')
   .controller('RankCtrl', function ($scope, $rootScope, $http, $window) {
     $http.get('/api/users' + '?' + $window.Math.random()).success(function (users) {
       var table = users;
-      table.forEach(function (u) { u.points = 0; u.historyPoints = []; u.historyRank = []; });
+      var avgs = [];
+      table.forEach(function (u) { u.points = 0; u.historyPoints = []; u.historyRank = []; u.historyAvgPoints = []; });
       $http.get('/api/predictions' + '?' + $window.Math.random()).success(function (matches) {
         matches.sort(function (a, b) { return a.matchnum - b.matchnum; })
           .filter(function (m) { return m.homeScore !== null; })
@@ -29,6 +30,13 @@ angular.module('worldcupApp')
                 }
                 r.historyPoints.push(r.points);
               });
+              var sum = table.map(function (a) { return a.historyPoints[a.historyPoints.length - 1]; })
+                .reduce(function (a, b) { return a + b ; });
+              var avg = sum / table.length;
+              avgs.push(avg);
+              table.forEach(function (u) {
+                u.historyAvgPoints.push(u.historyPoints[u.historyPoints.length - 1] - avg);
+              });
               var historyTable = table.sort(function (a, b) { return b.historyPoints[b.historyPoints.length - 1] - a.historyPoints[a.historyPoints.length - 1]; });
               for (var i = 0; i < historyTable.length; i++) {
                 var rank = i + 1;
@@ -42,12 +50,15 @@ angular.module('worldcupApp')
             }
           });
         $scope.table = table;
-        $scope.chartPoints = {
+        $scope.chartAvg = {
           options: {
             chart: {
               type: 'line'
             },
             tooltip: {
+              formatter: function() {
+                return '<b>Matchday: </b>' + (this.x + 1) + '<br><b>' + this.series.name + ': </b>' + this.y.toFixed(3);
+              },
               style: {
                 padding: 10
               }
@@ -56,13 +67,55 @@ angular.module('worldcupApp')
           title: {
             text: 'Historical Points'
           },
-          series: table.map(function (r, idx) {
+          series: [{
+            name: 'Average',
+            color: '#f00',
+            lineWidth: 5,
+            data: (function (length) {
+              var ret = [];
+              for (var i = 0; i < length; i++) {
+                ret.push(0);
+              }
+              return ret;
+            })(table[0].historyAvgPoints.length)
+          }].concat(table.map(function (r, idx) {
+            return {
+              name: r.name,
+              data: r.historyAvgPoints,
+              visible: idx < 5
+            };
+          }))
+        };
+
+        $scope.chartPoints = {
+          options: {
+            chart: {
+              type: 'line'
+            },
+            tooltip: {
+              formatter: function() {
+                return '<b>Matchday: </b>' + (this.x + 1) + '<br><b>' + this.series.name + ': </b>' + this.y;
+              },
+              style: {
+                padding: 10
+              }
+            },
+          },
+          title: {
+            text: 'Historical Points'
+          },
+          series: [{
+            name: 'Average',
+            data: avgs,
+            color: '#f00',
+            lineWidth: 5
+          }].concat(table.map(function (r, idx) {
             return {
               name: r.name,
               data: r.historyPoints,
               visible: idx < 5
             };
-          })
+          }))
         };
 
         $scope.chartRank = {
@@ -71,6 +124,9 @@ angular.module('worldcupApp')
               type: 'line'
             },
             tooltip: {
+              formatter: function() {
+                return '<b>Matchday: </b>' + (this.x + 1) + '<br><b>' + this.series.name + ': </b>' + this.y;
+              },
               style: {
                 padding: 10
               }
